97fe02913dad50ea8843cd0349bcbfda897705ec
sameerdewan <sameer.dewan@itential.com>
2021-03-10 17:44:49 -0500

    deploy ctx

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,16 @@
 const Poetry = artifacts.require('Poetry');
+const fs = require('fs');
 
 module.exports = function(deployer) {
-    deployer.deploy(Poetry, 'v1');
+    deployer.deploy(Poetry, 'v1')
+        .then(poetry => {
+            const contractData = {
+                address: poetry.address,
+                abi: poetry.abi
+            }
+            fs.writeFileSync('./contractData.json', JSON.stringify(contractData, null, '\t', 'utf-8'));
+            if (process.env.ENV === 'DEVELOPMENT') {
+                fs.writeFileSync('../../../appdata/contractData.json', JSON.stringify(contractData, null, '\t', 'utf-8'));
+            }
+        });
 }
