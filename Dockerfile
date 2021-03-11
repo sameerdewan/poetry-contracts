8ca9fb839f2cc6ccb01fcedcbd154ccdb63a3c22
sameerdewan <sameer.dewan@itential.com>
2021-03-11 16:48:15 -0500

    updates

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,6 @@ WORKDIR /usr/src/app
 
 EXPOSE 8545
 
-ENV MNEMONIC=squirrel\ march\ broom\ strong\ kick\ blast\ unique\ team\ song\ song\ assist\ million
-ENV INFURAKEY=https://rinkeby.infura.io/v3/df4de1f306044440bf0e4843d8ff667b
-ENV NETWORK=development
-ENV ENV=DEVELOPMENT
-
 RUN npm install -g ganache-cli
 
 CMD ["ganache-cli", "-m", "squirrel march broom strong kick blast unique team song song assist million"]
--- truffle-config.js
+++ truffle-config.js
@@ -1,10 +1,5 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 
-const {
-  MNEMONIC,
-  INFURAKEY,
-} = process.env;
-
 module.exports = {
   networks: {
     development: {
@@ -12,20 +7,20 @@ module.exports = {
       port: 8545,
       network_id: '*',
     },
-    rinkeby: {
-      provider: () => new HDWalletProvider(MNEMONIC, INFURAKEY),
-      network_id: 4,
-      gas: 8500000,
-      gasPrice: 10000000000,
-      type: 'quorum',
-    },
-    matic: {
-      provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.matic.today`),
+    mumbai: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, 'https://rpc-mumbai.matic.today'),
       network_id: 80001,
       confirmations: 2,
       timeoutBlocks: 200,
       skipDryRun: true
     },
+    matic: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, 'https://rpc-mainnet.matic.network'),
+      network_id: 137,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    }
   },
   compilers: {
     solc: {
