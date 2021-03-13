ba0f2f9db73b1d94db60a812eda209a541effb41
sameerdewan <sameer.dewan@itential.com>
2021-03-13 13:28:05 -0500

    test2

--- test/poetry.sol.test.js
+++ test/poetry.sol.test.js
@@ -2,6 +2,11 @@ const truffleAssert = require('truffle-assertions');
 const Poetry = artifacts.require('Poetry');
 
 contract('Poetry', async (accounts) => {
+    const owner = accounts[0];
+    const allowed = accounts[1];
+    const unallowed = accounts[2];
+    const version = 'v1';
+
     it('Poetry contract should require version field to deploy', async () => {
         let didNotDeploy = false;
         let errorMsg;
@@ -13,5 +18,13 @@ contract('Poetry', async (accounts) => {
         }
         assert.equal(didNotDeploy, true);
         assert.equal(errorMsg, 'Invalid number of parameters for "undefined". Got 0 expected 1!');
+    });
+    it('Poetry contract should deploy', async () => {
+        let didDeploy = false;
+        try {
+            await Poetry.new(version);
+            didDeploy = true;
+        } catch {}
+        assert.equal(didDeploy, true);
     });
 });
